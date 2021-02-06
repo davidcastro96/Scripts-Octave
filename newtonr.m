fea369b7cf32abfdcd3e71d577b02975c6035ce2
davidcastro96 <davidcastro0796@outlook.es>
2021-02-06 14:36:14 -0500

    Update

--- newtonr.m
+++ newtonr.m
@@ -1,44 +1,44 @@
-% Funció para calcular la raíz con el método Newton-Raphson.
+% FunciÃ³n para calcular la raÃ­z con el mÃ©todo Newton-Raphson.
 function newtonr
   
   clear
   clc
-  display('Método de Newton-Raphson')
+  display('MÃ©todo de Newton-Raphson')
   
   %-----------------------%
   % Registro de variables %
   %-----------------------%
   xi = 0.0; % Valor x_i
   error = 0.01; % Error
-  i = 1; % Iteración iniciada en 1
+  i = 1; % IteraciÃ³n iniciada en 1
   cant = 3;
 
-  % Función error aproximado
+  % FunciÃ³n error aproximado
   function ea = erroraprox(xa,xp)
     ea = abs(((xa-xp)/xa)*100);
   endfunction
   
 
   
-  %Función definida
+  %FunciÃ³n definida
   function[fun]=fn(x)
   fun = -100 + (160 / (1 + x)) + (165 / (1 + x)^2);
   return
   endfunction
 
-  %Derivada de la función definida
+  %Derivada de la funciÃ³n definida
   function[dr]=der(x)
   dr = -(160)/((1+x)^2) - (330)/((1+x)^3);
   return
   endfunction
   
   if (der(xi) == 0)
-    fprintf("No se puede hallar una raíz");
+    fprintf("No se puede hallar una raÃ­z");
   else
   
     xn = xi-(fn(xi)/der(xi));
     fprintf("%s\t\t %s\t\t %s\n","i","xn","ea");
-    fprintf("————————————————————————————————————————————\n");
+    fprintf("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\n");
     
     while i <= cant
       fprintf("%.0f\t\t",i);
@@ -56,6 +56,6 @@ function newtonr
   fprintf("%.8f\t",erroraprox(xn,xi));
   fprintf("\n");
 
-  fprintf("\nLa raíz aproximada es : %.3f\n",xn);
+  fprintf("\nLa raÃ­z aproximada es : %.3f\n",xn);
   
 endfunction
